07c3540bf017ba6846456b5bf1c71962f9bc12f1
Lucas Bonomo <bonomo.lucas@gmail.com>
2022-10-24 11:50:55 -0300

    Add path and fix storage

--- wp_update.sh
+++ wp_update.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-if []; then
-  wp_path='./'
-else 
-  wp_path='./'
+if [ ! $# -eq 0 ]; then
+    path=$1
+  else
+    path='./'
 fi
 
 # Backup directory.
-storage='~/varios/wordpress/plugins'
+storage=~/backup/wordpress/plugins
 [ ! -d $storage ] && mkdir -p $storage
 
 # Check if the command exists.
@@ -18,7 +18,7 @@ function cmd_checker {
   fi
 }
 
-# We need wp, jg and tar
+# We need wp, json_pp and tar
 commands="wp jq tar"
 for cmd in $commands
 do
@@ -27,7 +27,7 @@ done
 
 # List of plugins to update.
 # wp plugin list --allow-root --fields=name,version --update=available
-update=$(wp plugin list --allow-root --format=json --fields=name,version --update=available --path=`$wp_path`)
+update=$(wp plugin list --allow-root --format=json --fields=name,version --update=available --path=$path)
 
 if [ ! $? -eq 0 ]; then 
   exit
@@ -38,10 +38,10 @@ echo $update > update-$(date +%s).log
 
 echo $update | jq --raw-output 'map([.name, .version])[] | @tsv' |  while IFS=$'\t' read name version; do
   # Backup current plugin version
-  if [ ! -f $storage/$name\_$version.tar.gz ]; then
-    tar czf $storage/$name\_$version.tar.gz ./wp-content/plugins/$name
-  fi
+  # if [ ! -f $storage/$name\_$version.tar.gz ]; then
+  #   tar czf $storage/$name\_$version.tar.gz ./wp-content/plugins/$name
+  # fi
 
   # Update plugin
-  wp plugin update $name --allow-root --path=`$wp_path`
+  wp plugin update $name --allow-root --paht=$path
 done
